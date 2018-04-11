84e0f48eb93a7dd8e53b6d50fcf475afd560c9bb
hchesnutt <t.henry.chesnutt@gmail.com>
2018-04-11 16:12:06 -0700

    wireframe client files

---
+++ client/components/Reservations.jsx
@@ -0,0 +1,16 @@
+import react from 'react';
+import axios from 'axios';
+
+class Reservations extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+  render() {
+    <div>
+      Reservations rendered successfully!
+    </div>
+  }
+}
